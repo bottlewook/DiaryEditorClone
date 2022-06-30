cf26ebe8844a32a2f96a94d984573d6bae8798a6
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-06-30 22:33:40 +0900

    List_inquire

--- simplediaryclone/src/App.js
+++ simplediaryclone/src/App.js
@@ -1,10 +1,39 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DIaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "양현종",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+
+  {
+    id: 2,
+    author: "임병욱",
+    content: "하이 2",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+
+  {
+    id: 3,
+    author: "김소연",
+    content: "하이 3",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
