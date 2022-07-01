6fe3a845c3c05c6de480e71628b382caa16374ec
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-07-01 14:20:33 +0900

    again

--- simplediaryclone/src/App.js
+++ simplediaryclone/src/App.js
@@ -1,50 +1,11 @@
 import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
-import DiaryList from "./DIaryList";
-
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "양현종",
-//     content: "하이 1",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-
-//   {
-//     id: 2,
-//     author: "임병욱",
-//     content: "하이 2",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   },
-
-//   {
-//     id: 3,
-//     author: "김소연",
-//     content: "하이 3",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-// ];
 
 function App() {
-  const [data, setData] = useState([]);
-  const onCreate = (author, content, emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-    };
-    setData([newItem, ...data]);
-  };
   return (
     <div className="App">
-      <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryEditor />
     </div>
   );
 }
